78bcba5ea40902c73dd8dd4ebe9988960abd1aa3
Prince <Prince_Hacker@outlook.com>
2017-12-23 17:52:34 +0500

    update Calculate

---
+++ src/main/java/com/JAVA/learning/Calculate.java
@@ -0,0 +1,14 @@
+public class Calculate {
+  public static void main(String[] args) {
+
+    System.out.println("\n\nCalculate...");
+
+    int a = 12;
+    int b = 15;
+
+    int c = a + b;
+
+   System.out.println("Answer = " + c);
+
+  }
+}
